c26fa6a6c45df387405b2498c7bb4780bd4f89bc
pryority <matthewapryor@gmail.com>
2022-10-19 21:02:34 -0400

    _

--- app.py
+++ app.py
@@ -1,6 +1,41 @@
+from web3 import Web3, EthereumTesterProvider
 from web3 import Web3
+from dotenv import load_dotenv
+import os
 
-w3 = Web3(Web3.HTTPProvider(
-    'https://goerli.infura.io/v3/30e093dfb77d47589bda8b56ae38e7c1'))
+load_dotenv()
+
+my_address = os.getenv('WALLET_ADDRESS')
+infura_api = os.getenv('INFURA_API')
+private_key = os.getenv('PRIVATE_KEY')
+
+w3 = Web3(Web3.HTTPProvider(infura_api))
+
 result = w3.isConnected()
 print('Web3 is connected: ', result)
+
+# print(w3.eth.get_block('latest'))
+# print(w3.eth.get_balance(my_address))
+
+nonce = w3.eth.get_transaction_count(my_address)
+
+tx = {
+    'nonce': nonce,
+    'to': '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
+    'value': w3.toWei(1, 'ether'),
+    'gasPrice': w3.eth.gas_price
+}
+
+gas = w3.eth.estimate_gas(tx)
+tx['gas'] = gas
+print(tx)
+
+signed_tx = w3.eth.account.sign_transaction(tx, private_key)
+tx_hash = w3.eth.sendRawTransaction(signed_tx.rawTransaction)
+
+receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print(receipt)
+
+print('sender balance: ', w3.eth.get_balance(my_address))
+print('receiver balance: ', w3.eth.get_balance(
+    '0x8Ff966Ab0DadaDC70C901dD5cDc2C708d3A229AA'))
